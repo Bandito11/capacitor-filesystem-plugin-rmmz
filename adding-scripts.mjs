05d491f2e1831238855c73f721d661ac623e9877
Bandito11 <red_bandit_11@hotmail.com>
2023-11-23 21:50:36 -0400

    first commit

---
+++ adding-scripts.mjs
@@ -0,0 +1,13 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import * as cheerio from 'cheerio';
+
+const path = './game/index.html'
+const html = await readFile(path);
+
+const $ = cheerio.load(html);
+
+// Add more files as needed.
+$('body').append('<script type="module" src="js/native/utils.js"></script>');
+$('body').append('<script type="module" src="js/native/file.js"></script>');
+
+await writeFile(path, $('html').toString())
